ec58995c564def3153a3103e63c873f26f30ef2a
Markus Olsen <markusditlevolsen@gmail.com>
2023-01-19 12:26:45 +0100

    changed readme

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -110,22 +110,14 @@ def train(cfg):
     torch.save(model.state_dict(), "models/trained_model.pt")
     blob = bucket.blob("/trained_model.pt")
     blob.upload_from_filename("models/trained_model.pt")
-    print(
-        "Saved model files in " +
-        "'gs://model_checkpoints_group24/trained_model.pt"
-         )
+    print("Saved model files in " + "'gs://model_checkpoints_group24/trained_model.pt")
 
     # subprocess.check_call([
     #     'gsutil', 'cp', 'models/trained_model.pt',
     #     'gs://model_checkpoints_group24'
     # ])
 
-    print(
-        "Execution time:",
-        "{:5.2f}".format(
-            time.time() -
-            start_time),
-        "seconds")
+    print("Execution time:", "{:5.2f}".format(time.time() - start_time), "seconds")
 
 
 if __name__ == "__main__":
